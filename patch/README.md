4b68be7d920fd98cf0da6baf6608f9975a91cf75
Joey Xu <joey@Joeys-MacBook-Pro.local>
2025-10-21 20:01:28 +0800

    [feature]: add stub python interpreter patching script.

---
+++ patch/README.md
@@ -0,0 +1,5 @@
+# Patch specified `python` interpreter path within prebuilt DeepVariant zip binaries
+
+```bash
+python patch_dv_stub.py --zip /path/to/zip/binary --python /path/to/python3
+```
---
+++ patch/patch_dv_stub.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+find /opt/deepvariant/bin -type f -name '*.zip' -print0 |
+while IFS= read -r -d '' zip; do
+  python patch_dv_stub.py --zip "$zip" --python /usr/bin/python3
+done
