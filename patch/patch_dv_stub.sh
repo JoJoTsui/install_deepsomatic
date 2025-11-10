64af05677c4cd494a50999618f65f61c15e0ea34
Joey Xu <xuzy1992.joey@gmail.com>
2025-11-10 18:55:04 +0800

    [chore]: set PYTHON variable

--- patch/patch_dv_stub.sh
+++ patch/patch_dv_stub.sh
@@ -3,11 +3,13 @@
 # patched binary dir
 BIN_DIR="/t9k/mnt/hdd/work/Vax/deepvariant/patched_dv_1.9"
 # patched python3 path
+PYTHON="/t9k/mnt/joey/micromamba/envs/tf/bin/python3"
 
 
+# find every zipped binary in the BIN_DIR and patch
 find "${BIN_DIR}" -type f -name '*.zip' -print0 |
 while IFS= read -r -d '' zip; do
-  python3 patch_dv_stub.py --zip "$zip" --python /t9k/mnt/joey/micromamba/envs/tf/bin/python3
+  python3 patch_dv_stub.py --zip "$zip" --python "${PYTHON}"
 done
 
 # rsync to /opt/deepvariant/bin
